d813f6fbed7e61e2d679a6c0fb425dbaa1b13d48
cs-amit <csamit57@gmail.com>
2026-08-17 01:00:52 +0530

    feat: scaffold Expo Router app

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Today's Stories" }} />
+      <Stack.Screen name="story/[id]" options={{ title: "Story" }} />
+    </Stack>
+  );
+}
